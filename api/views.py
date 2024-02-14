21a02f93f805098b7bd1e63469c01c405dcbdd9d
Francisco André <franciscoandre.farp@gmail.com>
2024-02-14 10:43:09 +0000

    get serializer.dat

--- api/views.py
+++ api/views.py
@@ -1,7 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import IteamSerializer
 
 @api_view(['GET'])
 def getData(request):
-    person ={'name':'Max','number':33}
-    return Response(person)
+    items=Item.objects.all()
+    serializer=IteamSerializer(items,many=True)
+    return Response(serializer.data)
